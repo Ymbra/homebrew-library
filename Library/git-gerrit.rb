f5dd7d19fd950feeb4121b8e9216ec2f39cf3af0
pakmanlh <pako.garcia.rodriguez@gmail.com>
2015-07-08 08:40:08 +0200

    Update config.rb to sha1.

--- Library/git-gerrit.rb
+++ Library/git-gerrit.rb
@@ -3,7 +3,7 @@ require 'formula'
 class GitGerrit < Formula
   url 'https://github.com/Ymbra/git-gerrit/tarball/v0.4.0'
   homepage 'https://github.com/Ymbra/git-gerrit'
-  md5 'f0bb311eeebdb5d35860a884147239d9'
+  sha1 'ee0b8edae7bf11469901d4b556f67bfd32dbda65'
 
   def install
     # install scripts in bin.
